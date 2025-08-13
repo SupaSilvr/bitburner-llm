ea2c9ac38c139a924106f436a0ef195581143c83
SupaSilvr <36336923+SupaSilvr@users.noreply.github.com>
2025-08-12 21:30:07 -0400

    Update hack.js
    Updated for batching

--- hack.js
+++ hack.js
@@ -1,5 +1,7 @@
-/** @param {NS} ns */
+/** @param {NS} ns **/
 export async function main(ns) {
-  // Executes a hack against the target server passed as the first argument.
+  // args[0]: target server
+  // args[1]: time to sleep before executing
+  await ns.sleep(ns.args[1]);
   await ns.hack(ns.args[0]);
 }
